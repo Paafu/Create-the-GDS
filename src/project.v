bbf3b94d4b68fa1e3b07b06b56f94859b0910e49
Paafu <87361488+Paafu@users.noreply.github.com>
2026-03-19 16:33:24 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -692,7 +692,7 @@ end // initial
 `endif // SYNTHESIS
 endmodule
 
-module tt_um_example (
+module tt_um_delta (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
